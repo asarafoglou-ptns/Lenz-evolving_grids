84d251d3415e56294ddcd1f30b514364a5e086bc
sarah-lenz <lenz.soleil@gmail.com>
2024-05-28 12:09:38 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ setuptools.setup(
     version='1.0',
     author='sarah_lenz',
     author_email='sarah.lenz@student.uva.nl',
-    python_requires='>= 3.8',
+    python_requires='>=3.8',
     install_requires=[
         'shiny==0.9.0',
         'htmltools==0.5.1',
         'uvicorn==0.29.0',
         'starlette==0.37.2'
     ],
-    scripts=['app/run.py'],   # include run.py as a script
+    scripts=['app/run.py'],  # Include run.py as a script
     packages=setuptools.find_packages(),
     package_data={
-        'app': ['static/*.png', 'static/*.css'],  # include .png files and .css file within the static folder
-        '': ['requirements.txt']  # include requirements.txt at the root level
-    }
+        'app': ['static/*'],  # Include all files in the static folder
+    },
+    include_package_data=True,
 )
